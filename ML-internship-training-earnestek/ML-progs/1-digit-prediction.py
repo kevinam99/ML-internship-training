6899ac3b2acaed35a9fa581aeaa8eff6c6510df3
Kevin A Mathew <30661278+kevinam99@users.noreply.github.com>
2018-12-27 11:39:28 +0530

    Update 1-digit-prediction.py

--- ML-internship-training-earnestek/ML-progs/1-digit-prediction.py
+++ ML-internship-training-earnestek/ML-progs/1-digit-prediction.py
@@ -6,6 +6,11 @@ mnist = tf.keras.datasets.mnist
 
 #load dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+#For verification
+pyplot.imshow(x_train[10000], cmap = pyplot.cm.binary)
+pyplot.show()
+print(y_train[10000])
 #print(x_train) #Return RGB value of each picture
 
 #normalize the data
@@ -20,3 +25,15 @@ model.add(tf.keras.layers.Flatten(input_shape = (28,28)))
 
 #creating hidden layers
 model.add(tf.keras.layers.Dense(300, activation = tf.nn.relu )) #args(no. of neurons, activation function)
+#creating hidden layers
+model.add(tf.keras.layers.Dense(128, activation = tf.nn.relu )) #args(no. of neurons, activation function)
+model.add(tf.keras.layers.Dense(128, activation = tf.nn.relu ))
+model.add(tf.keras.layers.Dense(10, activation = tf.nn.softmax )) #10 neurons since there are only ten digits in total to be predicted
+
+model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])#compiles the neural network
+model.fit(x_train, y_train, epochs = 3)#gives the data and tells the neural network to run the number of epochs.
+
+val_loss, val_acc = model.evaluate(x_test, y_test)
+print(val_loss, val_acc)
+
+model.save('digit-prediction.model')
