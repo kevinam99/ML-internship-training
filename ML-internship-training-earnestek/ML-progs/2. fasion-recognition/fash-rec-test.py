19005f2d3f211cb48cc0be01bcbeccb09cc569f7
Kevin A Mathew <30661278+kevinam99@users.noreply.github.com>
2018-12-28 14:24:35 +0530

    Update fash-rec-test.py

--- ML-internship-training-earnestek/ML-progs/2. fasion-recognition/fash-rec-test.py
+++ ML-internship-training-earnestek/ML-progs/2. fasion-recognition/fash-rec-test.py
@@ -23,12 +23,14 @@ def apparel(argmaxPred):
 
 (x_train, y_train) ,(x_test, y_test) = fashion_mnist.load_data()
 
-pyplot.imshow(x_test[40], cmap = pyplot.cm.binary)
+test_image_number = 50 #This value must be changed for different pictures. Value can be anything between 0 and 59999
+
+pyplot.imshow(x_test[test_image_number], cmap = pyplot.cm.binary)
 pyplot.show()
 
 trainedModel = tf.keras.models.load_model('fash-rec.model')
 
 predictions = trainedModel.predict(x_test)
 
-argMax = np.argmax(predictions[40])
+argMax = np.argmax(predictions[test_image_number])
 print(f"The apparel is a {apparel(argMax)} muthafucka.")
