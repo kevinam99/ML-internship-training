e9376ec05f178e3e1446aa3ae8ce5fe27fc79881
Kevin A Mathew <30661278+kevinam99@users.noreply.github.com>
2018-12-29 10:34:57 +0530

    Update 1-digit-pred-test.py

--- ML-internship-training-earnestek/ML-progs/1. digit-recognition/1-digit-pred-test.py
+++ ML-internship-training-earnestek/ML-progs/1. digit-recognition/1-digit-pred-test.py
@@ -6,13 +6,15 @@ mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+test_image_number = 60 #Muat be changed for testing other images. The value can be anywhere between 0 and 59999
+
 #show image and the label (for my reference)
-pyplot.imshow(x_test[60], cmap = pyplot.cm.binary)
+pyplot.imshow(x_test[test_image_number], cmap = pyplot.cm.binary)
 pyplot.show()
-print(y_test[60])
+print(y_test[test_image_number])
 
 #load a previously trained model
 trainedModel = tf.keras.models.load_model('digit-prediction.model')
 
 predictions = trainedModel.predict([x_test])
-print(np.argmax(predictions[60]))#to find number with highest probability
+print(np.argmax(predictions[test_image_number]))#to find number with highest probability
